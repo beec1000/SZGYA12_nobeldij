9c57bc9b9fe4af7e28b5d6866dcab7422e50f7ab
beec <beec1000.minecraft@gmail.com>
2023-10-09 11:16:27 +0200

    Add project files.

---
+++ nobeldij/Nobeldijasok.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace nobeldij
+{
+    class Nobeldijasok
+    {
+        public int DijazasEve { get; set; }
+        public string Nev { get; set; }
+        public int SzuletesHalalozas { get; set; }
+        public string Orszagkod { get; set; }
+
+        public Nobeldijasok(string r)
+        {
+            string[] v = r.Split(';', '-');
+            this.DijazasEve = int.Parse(v[0]);
+            this.Nev = v[1];
+            this.SzuletesHalalozas = int.Parse(v[2]);
+            this.Orszagkod = v[3];
+        }
+
+
+    }
+}
---
+++ nobeldij/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace nobeldij
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Nobeldijasok> nobeldijasok = new List<Nobeldijasok>();
+
+            using var sr = new StreamReader(@"..\..\..\src\orvosi_nobeldijak.txt");
+            _ = sr.ReadLine();
+
+            while (!sr.EndOfStream)
+            {
+                nobeldijasok.Add(new Nobeldijasok(sr.ReadLine()));
+            }
+
+
+            Console.WriteLine($"3. feladat: \nDíjazottak száma: {nobeldijasok.Count} fő");
+
+
+            Console.WriteLine("4. feladat");
+
+            var f4 = nobeldijasok.OrderByDescending(k => k.DijazasEve).First();
+
+            Console.WriteLine($"Utolsó év: {f4}");
+
+            Console.WriteLine("5. feladat");
+
+        }
+    }
+}
