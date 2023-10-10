d6e4152ae90f52bb5679ac688079aef0ff1b83e9
beec1000 <120384928+beec1000@users.noreply.github.com>
2023-10-10 09:19:47 +0200

    Update Program.cs

--- nobeldij/Program.cs
+++ nobeldij/Program.cs
@@ -1,7 +1,8 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Text;
 
 namespace nobeldij
 {
@@ -9,27 +10,33 @@ namespace nobeldij
     {
         static void Main(string[] args)
         {
-            List<Nobeldijasok> nobeldijasok = new List<Nobeldijasok>();
+            List<Nobeldijasok> dijazottak = new List<Nobeldijasok>();
 
-            using var sr = new StreamReader(@"..\..\..\src\orvosi_nobeldijak.txt");
+            using var sr = new StreamReader(@"..\..\..\src\orvosi_nobeldijak.txt",
+                encoding: Encoding.UTF8);
             _ = sr.ReadLine();
 
             while (!sr.EndOfStream)
             {
-                nobeldijasok.Add(new Nobeldijasok(sr.ReadLine()));
+                dijazottak.Add(new Nobeldijasok(sr.ReadLine()));
             }
 
 
-            Console.WriteLine($"3. feladat: \nDíjazottak száma: {nobeldijasok.Count} fő");
-
+            Console.WriteLine($"3. feladat: \nDíjazottak száma: {dijazottak.Count} fő");
 
             Console.WriteLine("4. feladat");
 
-            var f4 = nobeldijasok.OrderByDescending(k => k.DijazasEve).First();
+            var f4 = dijazottak.Max(k => k.Ev);
 
             Console.WriteLine($"Utolsó év: {f4}");
 
             Console.WriteLine("5. feladat");
+            Console.WriteLine("Kérem adja meg egy ország kódját: ");
+            var orK = Console.ReadLine();
+
+            var f5 = dijazottak.Where(k => k.Orszag == orK);
+
+            Console.WriteLine($"A megadott ország díjazottja: \nNév: {} \nÉv: {} \nSz/H: {}");
 
         }
     }
